5a58cde85b51257c1e4df89bd5cb760e50cabfe1
Kelly Collins <90kcol@gmail.com>
2020-03-03 11:49:29 -0500

    created model for ingredients and recipe.  Added forms and buttons to shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
@@ -18,7 +17,6 @@ import { RecipeComponent } from './recipe/recipe.component';
     HeaderComponent,
     ShoppingListComponent,
     RecipeListComponent,
-    IngredientComponent,
     ShoppingEditComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
